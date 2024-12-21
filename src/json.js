8e301a9332d3a151f9f0384086d87621ccc24ee4
nicholas harper <nicholasharper000@gmail.com>
2024-12-21 03:31:10 -0800

    Update json.js

--- src/json.js
+++ src/json.js
@@ -1,28 +1,36 @@
-var Json = {
+var Json = 
+{
   activeRequestCount: 0,
   currentRequest: false
 };
 
-Json.Responders = {
+Json.Responders = 
+{
   responders: [],
   
-  _each: function(iterator) {
+  _each: function(iterator) 
+  {
     this.responders._each(iterator);
   },
 
-  register: function(responder) {
+  register: function(responder)
+  {
     if (!this.include(responder))
       this.responders.push(responder);
   },
   
-  unregister: function(responder) {
+  unregister: function(responder) 
+  {
     this.responders = this.responders.without(responder);
   },
   
-  dispatch: function(callback, request, json) {
+  dispatch: function(callback, request, json) 
+  {
     this.each(function(responder) {
-      if (Object.isFunction(responder[callback])) {
-        try {
+      if (Object.isFunction(responder[callback])) 
+      {
+        try
+        {
           responder[callback].apply(responder, [request, json]);
         } catch (e) { }
       }
@@ -33,22 +41,26 @@ Json.Responders = {
 Object.extend(Json.Responders, Enumerable);
 
 Json.Responders.register({
-  onCreate:   function() {
+  onCreate:   function() 
+  {
     Json.activeRequestCount++;
   }, 
-  onComplete: function(request,json) {
+  onComplete: function(request,json) 
+  {
     Json.activeRequestCount--;
     request.response = json;
   }
 });
 
-//Json.callback(JSON) is used to store the JSON
 Object.extend(Json, {
   callback: function(json) {
-    try {
+    try 
+    {
       Json.Responders.dispatch('onComplete', Json.currentRequest, json);
       (Json.currentRequest.options.get('onComplete') || Prototype.emptyFunction)(Json.currentRequest, json);
-    } catch (e) {
+    } 
+    catch (e) 
+    {
       Json.currentRequest.dispatchException(e);
     }
   }
@@ -57,14 +69,15 @@ Object.extend(Json, {
 Json.Request = Class.create({
   initialize: function(url, options) {
     this.options = $H({
-      //destroy()
     }).update($H(options));
     Json.currentRequest = this;
     this.request(url);
   },
 
-  request: function(url) {
-    try {
+  request: function(url) 
+  {
+    try 
+    {
       (this.options.get('onCreate') || Prototype.emptyFunction)(this);
       Json.Responders.dispatch('onCreate', this);
       var head = $$('head')[0];                 
@@ -74,12 +87,14 @@ Json.Request = Class.create({
       script.src = url+'&callback=Json.callback'+ '&cachebuster=' + (new Date() * 1)
       head.appendChild(script);
     }
-    catch (e) {
+    catch (e)
+    {
       this.dispatchException(e);
     }
   },
 
-  dispatchException: function(exception) {
+  dispatchException: function(exception)
+    {
     (this.options.get('onException') || Prototype.emptyFunction)(this, exception);
     Json.Responders.dispatch('onException', this, exception);
   }
